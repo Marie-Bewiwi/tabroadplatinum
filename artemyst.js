880ce9178e9be4fc596ef11bb1eac8a2f55f1f95
Marie-Angélique Sommier <marie.a.s45@gmail.com>
2021-06-15 21:04:24 +0200

    modified html file and parsed cells

--- artemyst.js
+++ artemyst.js
@@ -3,9 +3,17 @@ const fsp = require('fs').promises
 function parse(content) {
     const doc = (new JSDOM(content)).window.document
     const table = doc.querySelector('table')
-    console.log(table)
+    const rows = table.querySelectorAll('tr')
+    console.log(`${rows.length} rows found`) // = concaténation
+    rows.forEach(function parserow(row) {
+        const colonnes = row.querySelectorAll('td')
+        colonnes.forEach(function parsecolonnes(cellule) {
+            console.log(cellule.textContent)
+        })
+    })
+
 };
 function onerror() {
     console.log('cant read file');
 }
-fsp.readFile("./page.html", { encoding: 'UTF8' }).then(parse).catch(onerror)
+fsp.readFile("./artemyst.html", { encoding: 'UTF8' }).then(parse).catch(onerror)
